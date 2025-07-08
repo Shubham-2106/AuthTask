4a3caf5903d3b178dd1fa30483eb21518545bee6
Shubham-2106 <shubham.96675@gmail.com>
2025-07-08 19:38:16 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ token: generateToken(user._id) });
+  } catch (err) {
+    res.status(400).json({ message: "Signup failed", error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && await user.matchPassword(password)) {
+      res.json({ token: generateToken(user._id) });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
